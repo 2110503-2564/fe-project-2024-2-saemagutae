54f8f5f4b8e3aaddc4dc3fac7f05c05e1e60646b
JingKritsada <jing.kritsada@gmail.com>
2025-03-22 17:54:18 +0700

    Set up for other page

---
+++ src/app/booking/page.tsx
@@ -0,0 +1,14 @@
+export default function AboutPage() {
+    return (
+        <main>
+            <h1>Your Reservation</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et
+                metus nec libero ultrices fermentum. Nullam ac nunc nec purus
+                tincidunt congue. Proin et nisl nec nunc fermentum aliquam.
+                Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+                posuere cubilia curae; Sed nec ultricies velit
+            </p>
+        </main>
+    );
+}
---
+++ src/app/reservation/page.tsx
@@ -0,0 +1,14 @@
+export default function ReservationPage() {
+    return (
+        <main>
+            <h1>Reserve Co-woking Space</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et
+                metus nec libero ultrices fermentum. Nullam ac nunc nec purus
+                tincidunt congue. Proin et nisl nec nunc fermentum aliquam.
+                Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+                posuere cubilia curae; Sed nec ultricies velit
+            </p>
+        </main>
+    );
+}
